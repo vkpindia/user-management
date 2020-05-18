e7665518ee3dc56de68df22d153d60a64e756ab8
vkpindia <infovkpg@gmail.com>
2020-05-18 09:40:06 +0530

    Service  and font-awesome integration

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -18,7 +18,7 @@ export class UserService {
     headers: new HttpHeaders({ 'content-type': 'application/json' })
   };
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
 
   /**
    * @description Method to getting error if occured
@@ -56,5 +56,79 @@ export class UserService {
     const body = res;
     return body || {};
   }
+
+  /**
+   * @description Method for getting user from server
+   * @author Virendra Pandey
+   * @date 2020-05-17
+   * @returns {Observable<any>}
+   * @memberof UserService
+   */
+  public getUsers(): Observable<any> {
+    return this._http.get(this.apiUrl, this.httpOptions).pipe(
+      map(this.extractData),
+      catchError(this.handleError));
+  }
+
+  /**
+   * @description Method for getting unique record by ID
+   * @author Virendra Pandey
+   * @date 2020-05-17
+   * @param {number} userID
+   * @returns {Observable<any>}
+   * @memberof UserService
+   */
+  public getUserByID(userID: number): Observable<any> {
+    // http://localhost:3000/data/1024
+    const url = `${this.apiUrl}/${userID}`;
+    return this._http.get(url, this.httpOptions).pipe(
+      map(this.extractData),
+      catchError(this.handleError));
+  }
+
+  /**
+   * @description Method for posting/sending data to server
+   * @author Virendra Pandey
+   * @date 2020-05-17
+   * @param {*} userRecord
+   * @returns {Observable<any>}
+   * @memberof UserService
+   */
+  public postUser(userRecord: any): Observable<any> {
+    return this._http.post(this.apiUrl, userRecord, this.httpOptions).pipe(
+      map(this.extractData),
+      catchError(this.handleError));
+  }
+
+  /**
+   * @description Method for updating data to server
+   * @author Virendra Pandey
+   * @date 2020-05-17
+   * @param {*} userRecord
+   * @returns {Observable<any>}
+   * @memberof UserService
+   */
+  public updateUser(userRecord: any): Observable<any> {
+    return this._http.put(this.apiUrl, userRecord, this.httpOptions).pipe(
+      map(this.extractData),
+      catchError(this.handleError));
+  }
+
+  /**
+   * @description Method for deleting user record
+   * @author Virendra Pandey
+   * @date 2020-05-17
+   * @param {*} userRecord
+   * @returns {Observable<any>}
+   * @memberof UserService
+   */
+  public deleteUser(userID: number): Observable<any> {
+    // http://localhost:3000/data/1024
+    const url = `${this.apiUrl}/${userID}`;
+    return this._http.delete(url, this.httpOptions).pipe(
+      map(this.extractData),
+      catchError(this.handleError));
+  }
+
 
 }
--- src/app/user/user-list/user-list.component.ts
+++ src/app/user/user-list/user-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services';
 
 @Component({
   selector: 'app-user-list',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserListComponent implements OnInit {
 
-  constructor() { }
+  public usersList: Array<any>;
+  constructor(private _userService: UserService) { }
 
   ngOnInit(): void {
+    // method call
+    this.getUserList();
   }
 
+  /**
+   * @description Method to get users list
+   * @author Virendra Pandey
+   * @date 2020-05-17
+   * @memberof UserListComponent
+   */
+  public getUserList(): void {
+    this._userService.getUsers().subscribe(res => {
+      if (res) {
+        this.usersList = res;
+        console.log('this.usersList', this.usersList);
+      }
+    }, err => {
+      console.log(err);
+    });
+  }
 }
